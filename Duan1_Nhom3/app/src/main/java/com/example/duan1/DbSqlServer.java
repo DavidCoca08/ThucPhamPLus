81a41b0b38628f4dddfa1d0ac5ac037111223742
quang26071999 <108580841+quang26071999@users.noreply.github.com>
2022-07-13 22:14:15 +0700

    duan1

---
+++ Duan1_Nhom3/app/src/main/java/com/example/duan1/DbSqlServer.java
@@ -0,0 +1,40 @@
+package com.example.duan1;
+
+
+import android.os.StrictMode;
+import android.util.Log;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class  DbSqlServer {
+
+    Connection connection = null;
+    final String TAG = "zzzzzz";
+
+    public Connection openConnect(){
+        String ip = "192.168.1.101",
+                port = "1433",
+                user = "sa",
+                pass = "123",
+                db = "demo";
+        StrictMode.ThreadPolicy threadPolicy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(threadPolicy);
+
+        try {
+            Class.forName("net.sourceforge.jtds.jdbc.Driver");
+            String connectUrl = "jdbc:jtds:sqlserver://" + ip + ":" + port + ";databasename=" + db +";user=" + user +";password=" + pass +";";
+            connection = DriverManager.getConnection(connectUrl);
+            Log.d(TAG, "openConnect: OK");
+
+
+        } catch (Exception e) {
+            Log.e(TAG, "getCollection: Loi ket noi CSDL" );
+            e.printStackTrace();
+        }
+        return connection;
+    }
+
+
+}
+
