bdc6db35c3d328190c152ab9e4bcb5ddd8cd3dc1
MinhTran <tminh400@gmail.com>
2022-07-19 16:57:11 +0700

    up file SignInByPhoneActivity
    up files java, xml
    sửa file manifest

---
+++ ThucPhamXanh/app/src/main/java/com/example/thucphamxanh/Activity/SignInByPhoneActivity.java
@@ -0,0 +1,16 @@
+package com.example.thucphamxanh.Activity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.thucphamxanh.R;
+
+public class SignInByPhoneActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in_by_phone);
+    }
+}
