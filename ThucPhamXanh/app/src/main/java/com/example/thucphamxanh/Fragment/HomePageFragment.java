c9166652f3dc4819b7dd6cb44993515db34e56a7
HienBull98 <phunghien9812@gmail.com>
2022-07-27 00:01:02 +0700

    homePage

--- ThucPhamXanh/app/src/main/java/com/example/thucphamxanh/Fragment/HomePageFragment.java
+++ ThucPhamXanh/app/src/main/java/com/example/thucphamxanh/Fragment/HomePageFragment.java
@@ -3,15 +3,21 @@ package com.example.thucphamxanh.Fragment;
 import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.example.thucphamxanh.Adapter.FavoriteItemAdapter;
+import com.example.thucphamxanh.Fragment.ProductFragments.FruitFragment;
+import com.example.thucphamxanh.Fragment.ProductFragments.MeatFragment;
 import com.example.thucphamxanh.Fragment.ProductFragments.VegetableFragment;
 import com.example.thucphamxanh.Model.FavoviteModel;
 import com.example.thucphamxanh.R;
@@ -26,6 +32,8 @@ public class HomePageFragment extends Fragment {
     private List<FavoviteModel> favoviteModelList;
     private FavoriteItemAdapter favoriteItemAdapter;
 
+    CardView card_vegetable_home,card_fruit_home,card_meat_home,card_food_home;
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -33,6 +41,10 @@ public class HomePageFragment extends Fragment {
 
         View view = inflater.inflate(R.layout.fragment_home_page, container, false);
 
+        card_vegetable_home = view.findViewById(R.id.card_vegetable_home);
+        card_fruit_home = view.findViewById(R.id.card_fruit_home);
+        card_meat_home = view.findViewById(R.id.card_meat_home);
+        card_food_home = view.findViewById(R.id.card_food_home);
 
         favorite_recyclerView_home = view.findViewById(R.id.favorite_recyclerView_home);
         favorite_recyclerView_home.setLayoutManager(new LinearLayoutManager(getContext()));
@@ -70,9 +82,40 @@ public class HomePageFragment extends Fragment {
         favoriteItemAdapter = new FavoriteItemAdapter(favoviteModelList);
         favorite_recyclerView_home.setAdapter(favoriteItemAdapter);
 
+        card_vegetable_home.setOnClickListener(view1 -> {
+//           VegetableFragment vegetableFragment = new VegetableFragment();
+//            FragmentTransaction fragmentTransaction = getActivity().getSupportFragmentManager().beginTransaction();
+//
+//            fragmentTransaction.replace(R.id.frame_Home,vegetableFragment);
+//            fragmentTransaction.addToBackStack(null);
+//
+//            fragmentTransaction.commit();
+
+            replaceFragment(new VegetableFragment());
+        });
+        card_fruit_home.setOnClickListener(view1 -> {
+            replaceFragment(new FruitFragment());
+        });
+        card_meat_home.setOnClickListener(view1 -> {
+            replaceFragment(new MeatFragment());
+        });
+        card_food_home.setOnClickListener(view1 -> {
+            Toast.makeText(getActivity(), "Đồ ăn", Toast.LENGTH_SHORT).show();
+        });
+
 
         return view;
     }
+
+    private  void replaceFragment(Fragment fragment){
+        FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_Home,fragment);
+        fragmentTransaction.commit();
+    }
+
+
+
 
 
 }
