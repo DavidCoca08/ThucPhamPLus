fe7b3b08e20ad36b49e6775f14f75e52f74937d5
thehung2001 <hungnt201x@gmail.com>
2022-08-05 20:21:30 +0700

    update

--- ThucPhamXanh/app/src/main/java/com/example/thucphamxanh/Fragment/PersonalFragment.java
+++ ThucPhamXanh/app/src/main/java/com/example/thucphamxanh/Fragment/PersonalFragment.java
@@ -9,7 +9,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.thucphamxanh.Activity.MainActivity;
 import com.example.thucphamxanh.Activity.SignInActivity;
@@ -22,6 +24,7 @@ public class PersonalFragment extends Fragment {
     FragmentPersonalBinding binding;
     Button btn_logout_personal, btn_changepassword_personal;
     TextView tvNumberPhoneUser, tvEdit;
+    ImageView imgUser;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -33,12 +36,16 @@ public class PersonalFragment extends Fragment {
         btn_changepassword_personal = binding.btnPersonalFragmentChangePasswordPersonal;
         tvNumberPhoneUser = binding.tvPersonalFragmentNumberPhoneUser;
         tvEdit = binding.tvPersonalFragmentEditUser;
-
+        imgUser = binding.imgPersonalFragmentImgUser;
+        imgUser.setImageResource(R.drawable.ic_avatar_default);
         tvEdit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
             }
+        });
+        imgUser.setOnClickListener(view -> {
+            Toast.makeText(getContext(), "imgUser.getS00ourceLayoutResId()",Toast.LENGTH_SHORT).show();
         });
 
         btn_logout_personal.setOnClickListener(new View.OnClickListener() {
